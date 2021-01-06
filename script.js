d0f2478b2bf4195ed3ea620304e1376171547dd3
LaurenJeffrey <lauren.jeffrey@ocsbstudent.ca>
2021-01-06 15:48:14 +0000

    js

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function myfunction () {
   const day = document.getElementById('daytyped').value
   const age = document.getElementById('agetyped').value
   if ((day === 'Tuesday' || day === 'Thursday') && (age > 12 && age < 21)) {
-    alert('You are eligible for the student discount!')}
+    alert('You are eligible for the student discount!') }
   else if (age < 5 || age > 95) {
-    alert('You are eligible for free entry!')}
+    alert('You are eligible for free entry!') }
   else {
-    alert('You must pay regular price to be admitted!')}
+    alert('You must pay regular price to be admitted!') }
 }
