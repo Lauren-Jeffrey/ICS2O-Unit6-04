6e7835b12402d9255e69bb454f83096746ddf952
LaurenJeffrey <lauren.jeffrey@ocsbstudent.ca>
2021-01-06 01:29:38 +0000

    js

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
- document.getElementById("determine").addEventListener("click", myfunction);
-    function myfunction () {
-    let day;
-    let age;
-    day = document.getElementById("daytyped").value;
-    age = document.getElementById("agetyped").value;
-    
-    if ((day == "Tuesday" || day == "Thursday") && (age > 12 && age < 21)) {
+document.getElementById('determine').addEventListener('click', myfunction)
+function myfunction () {
+  let day = document.getElementById('daytyped').value
+  let age = document.getElementById('agetyped').value
+
+  if ((day == 'Tuesday' || day == 'Thursday') && (age > 12 && age < 21)) {
       alert('You are eligible for the student discount!')
     }
     else if (age < 5 || age > 95) {
       alert ('You are eligible for free entry!');
     }
-     else if (age > 20 && age < 94) {
+     else {
        alert ('You must pay regular price to be admitted!')
      }
     }
